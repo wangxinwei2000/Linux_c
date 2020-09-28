3e02d1c1f16f9741bdd3312c1181568539bba99a
wangxinwei2000 <2087201681@qq.com>
2020-09-28 17:58:38 +0800

    9月28日修改

--- test1.c
+++ test1.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 extern  void Print();
-int main(){
-  printf("the frist project!\n");
-  int n=10;
-  Print(10);
-  return 0;
+int main()
+{
+    int a[6]={1,23,4,56,6};
+    int i,j,locat;
+    printf("please put the location of inserting:\t");
+    scanf("%d",&locat);
+    printf("please put the insert number:\n");
+    scanf("%d",&j);
+    for(i=5;i>locat;i--)
+        *(a+i)=*(a+i-1);
+    *(a+locat-1)=j;
+    for(i=0;i<5;i++)
+        printf("%d\t",a[i]);
+    return 0;
 }
